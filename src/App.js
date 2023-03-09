6dac9c1838285657fc7d4815452b857502082b82
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-03-09 19:21:24 -0300

    Components Game and GameOver

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 
+//HOOKS
+import { useCallback, useEffect, useState } from 'react'
+
+//DATA
+import { wordsList } from "./data/words"
+
 //COMPONENTS
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+//STAGES
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage == 'game' && <Game/>}
+      {gameStage == 'end' && <GameOver/>}
     </div>
   );
 }
